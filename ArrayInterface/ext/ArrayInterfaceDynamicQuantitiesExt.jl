c1c39ae7b646fb8aa69ba22566d4d57491e63caf
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-02-15 14:16:14 +0000

    fix: add ArrayInterface DQ extension (lu_instance/qr_instance)

---
+++ ArrayInterface/ext/ArrayInterfaceDynamicQuantitiesExt.jl
@@ -0,0 +1,39 @@
+module ArrayInterfaceDynamicQuantitiesExt
+
+using ArrayInterface
+using DynamicQuantities
+using LinearAlgebra
+
+# DynamicQuantities quantities have runtime-stored dimensions, so identity elements
+# like `zero(::Type{<:Quantity})` / `oneunit(::Type{<:Quantity})` are intentionally
+# undefined. ArrayInterface's instance constructors should therefore avoid calling
+# `zeros(T, ...)` on Quantity element types.
+
+function ArrayInterface.qr_instance(A::Matrix{T}, pivot = ArrayInterface.DEFAULT_CHOLESKY_PIVOT) where {T <: DynamicQuantities.UnionAbstractQuantity}
+    if pivot === ArrayInterface.DEFAULT_CHOLESKY_PIVOT
+        return LinearAlgebra.QRCompactWY(Matrix{T}(undef, 0, 0), Matrix{T}(undef, 0, 0))
+    else
+        return LinearAlgebra.QRPivoted(Matrix{T}(undef, 0, 0), Vector{T}(undef, 0), Vector{Int}(undef, 0))
+    end
+end
+
+function ArrayInterface.svd_instance(A::Matrix{T}) where {T <: DynamicQuantities.UnionAbstractQuantity}
+    return LinearAlgebra.SVD(
+        Matrix{T}(undef, 0, 0),
+        Vector{real(T)}(undef, 0),
+        Matrix{T}(undef, 0, 0),
+    )
+end
+
+# Provide a cheap LU "instance" for Quantity matrices without calling `lu(A)`.
+# This is used for cache allocation; actual factorization should be done elsewhere.
+function ArrayInterface.lu_instance(A::Matrix{T}) where {T <: DynamicQuantities.UnionAbstractQuantity}
+    isempty(A) && return ArrayInterface.lu_instance(Matrix{Float64}(undef, 0, 0))
+    noUnitT = typeof(zero(DynamicQuantities.ustrip(first(A))))
+    luT = LinearAlgebra.lutype(noUnitT)
+    ipiv = Vector{LinearAlgebra.BlasInt}(undef, 0)
+    info = zero(LinearAlgebra.BlasInt)
+    return LinearAlgebra.LU{luT}(similar(A, 0, 0), ipiv, info)
+end
+
+end
